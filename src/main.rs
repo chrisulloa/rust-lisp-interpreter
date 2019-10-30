e5bfea7987307c8830f887aa9716ad29c0a537c5
Christian <chrisulloago@gmail.com>
2019-10-30 15:19:45 -0700

    Added tokenize function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+fn tokenize(expression: &str) -> Vec<String> {
+    return expression
+        .replace("(", " ( ")
+        .replace(")", " ) ")
+        .split(" ")
+        .map(|s| s.to_string())
+        .collect();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let program: &str = "(begin (define r 10) (* r r)";
+    println!("{}", program);
+    let tokenized: Vec<String> = tokenize(program);
+
+    for t in tokenized {
+        println!("{}", t)
+    }
 }
